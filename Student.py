d24cfa54dc4c1fc7e78e0d8579b69c58328ac91d
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-01-15 14:53:01 +0300

    Created Student class

---
+++ Student.py
@@ -0,0 +1,9 @@
+class Student:
+
+    def __init__(self,number,name,surname,description,email,submissions):
+        self.__number=number
+        self.__name=name
+        self.__surname=surname
+        self.__description=description
+        self.__email=email
+        self.__submissions=submissions
