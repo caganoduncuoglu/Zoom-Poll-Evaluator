ab7b0d7cd70a20839e0f5f95404527ae09eb8b82
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-01-15 15:18:15 +0300

    Updated Student class

--- Student.py
+++ Student.py
@@ -1,9 +1,8 @@
 class Student:
 
-    def __init__(self,number,name,surname,description,email,submissions):
+    def __init__(self,number,name,surname,description,submissions=[]):
         self.__number=number
         self.__name=name
         self.__surname=surname
         self.__description=description
-        self.__email=email
         self.__submissions=submissions
