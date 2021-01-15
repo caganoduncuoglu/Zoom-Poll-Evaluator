f1e493529008175962db8073d77afce2cc5754e2
osmanerikci <48260172+osmanerikci@users.noreply.github.com>
2021-01-15 15:24:13 +0300

    Answer,Poll,Question classes have been added.

---
+++ entities/Answer.py
@@ -0,0 +1,5 @@
+class Answer(object):
+
+    def __init__(self, description, question):
+        self.__description = description
+        self.__question = question
---
+++ entities/Question.py
@@ -0,0 +1,9 @@
+class Question(object):
+
+    _all_answers = []
+
+    def __init(self, description, true_answer, poll):
+
+        self.__description = description
+        self.__true_answer = true_answer
+        self.__poll = poll
