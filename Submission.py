7831755a115e2f8f16fa93aff1e1c6c4cdce3b9d
aEmreAydemir <45498427+aEmreAydemir@users.noreply.github.com>
2021-01-15 14:55:44 +0300

    Created Submission class

---
+++ Submission.py
@@ -0,0 +1,7 @@
+class Submission:
+
+    def __init__(self,studentAnswers,submittedDatetime,student,poll):
+        self.__studentAnswers=studentAnswers
+        self.__submittedDatetime=submittedDatetime
+        self.__student=student
+        self.__poll=poll
