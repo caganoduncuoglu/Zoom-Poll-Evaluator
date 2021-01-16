0fba0c80e2f055c07398ef72a93ad9e34fc5e33d
talhabayburtlu <talha_bayburtlu2@hotmail.com>
2021-01-16 12:05:43 +0300

    Added question and it's true answer while creating poll.

--- creators/PollCreator.py
+++ creators/PollCreator.py
@@ -1,4 +1,6 @@
+from entities.Answer import Answer
 from entities.Poll import Poll
+from entities.Question import Question
 from utils.Singleton import Singleton
 
 
@@ -6,7 +8,15 @@ class PollCreator(metaclass=Singleton):
     def __init__(self):
         self.polls = []
 
-    def create_poll(self, name):
+    def create_poll(self, name, question, answer):
         poll = Poll(name)
+        # TODO: Might an error occur here because of a warning, look here later.
+        new_question = Question(question, None, poll, None)
+        new_answer = Answer(answer, new_question)
+
+        new_question.true_answer = new_answer
+        new_question.all_answers.append(new_answer)
+
+        poll.poll_questions.append(new_question)
         self.polls.append(poll)
         return poll
