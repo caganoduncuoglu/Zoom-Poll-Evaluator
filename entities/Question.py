eb77fa20b15904d721c36cd18d4a744852001d1d
zekeriyaBatuhanKaratas <zbatuhankaratas@gmail.com>
2021-01-16 17:07:16 +0300

    number_of_answer_selection variable has been added to Question class.

--- entities/Question.py
+++ entities/Question.py
@@ -1,9 +1,10 @@
 class Question(object):
 
-    def __init__(self, description, true_answer, poll, all_answers=None):
+    def __init__(self, description, true_answer, poll, all_answers=None, number_of_answer_selection=None):
         if all_answers is None:
             all_answers = []
-
+        if number_of_answer_selection is None:
+            number_of_answer_selection = []
         self.description = description
         self.true_answer = true_answer
         self.poll = poll
