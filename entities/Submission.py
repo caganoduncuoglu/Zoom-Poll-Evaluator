554a62153c702d1f51a3d700d3e767442e6eaf41
zekeriyaBatuhanKaratas <zbatuhankaratas@gmail.com>
2021-01-16 19:41:37 +0300

    Submission class has been rolled back.

--- entities/Submission.py
+++ entities/Submission.py
@@ -1,10 +1,7 @@
 class Submission:
 
-    def __init__(self, submitted_datetime, student, poll, student_answers = None):
-        if student_answers is None:
-            student_answers = []
+    def __init__(self, student_answers, submitted_datetime, student, poll):
         self.student_answers = student_answers
-        #there should be a check with question answers for Number of answer Selection
         self.submitted_datetime = submitted_datetime
         self.student = student
         self.poll = poll
