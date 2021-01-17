d25acb70f8f8a5dc965e769769570c70b64288c8
Can Karatepe <cankaratepe2000@gmail.com>
2021-01-17 22:52:11 +0300

    Updates to main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import os
+
 from pandas import DataFrame
 
+from creators.AttendanceCreator import AttendanceCreator
 from utils.ExcelParser import ExcelParser
 
 from creators.StudentCreator import StudentCreator
 from creators.PollCreator import PollCreator
 from creators.SubmissionCreator import SubmissionCreator
+from utils.JsonParser import JsonParser
 
 ExcelParser().read_students("CES3063_Fall2020_rptSinifListesi.XLS")
 print(StudentCreator().students)
 ExcelParser().read_key("design_poll.csv")
-ExcelParser().read_key("development_poll.csv")
+# ExcelParser().read_key("development_poll.csv")
 print(PollCreator().polls)
-ExcelParser().read_submissions("CSE3063_20201116_Mon_zoom_PollReport.csv")
+attendancefilename = "attendances.json"
+if os.path.exists(attendancefilename):
+    JsonParser().read_attendances(StudentCreator().students, attendancefilename)
+ExcelParser().read_submissions("CSE3063_20201123_Mon_zoom_PollReport.csv")
 print(SubmissionCreator().submissions)
+JsonParser().write_attendances(AttendanceCreator().attendances, attendancefilename)
 ExcelParser().write_all_students(StudentCreator().students)
 poll_count = 1
 for poll in PollCreator().polls:
